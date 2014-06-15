b56cb6affbe35359d97b93254787491e88cb9b8b
Henrik Joreteg <hjoreteg@gmail.com>
2014-06-15 01:35:37 -0700

    now with imported content

--- build.js
+++ build.js
@@ -1,21 +1,17 @@
+var pack = require('./package.json');
 var fs = require('fs');
 var request = require('request');
 var jade = require('jade');
 var marked = require('marked');
 var async = require('async');
-var generateHtml = require('./lib/html');
-var generateJSON = require('./lib/json');
 var rmrf = require('rimraf');
 var templateGlobals = {};
-var jadeTemplate = fs.readFileSync(__dirname + '/index.jade', 'utf8');
+var jadeTemplate = fs.readFileSync(__dirname + '/docs/index.jade', 'utf8');
 var includes = [];
 var intro = marked(fs.readFileSync(__dirname + '/intro.md', 'utf8'));
+var getModules = require('./lib/get-modules');
 
 console.log("INTRO", intro.length);
-
-// dump json folder
-rmrf.sync(__dirname + '/json');
-fs.mkdirSync(__dirname + '/json');
 
 // replace include lines with
 jadeTemplate = jadeTemplate.replace(/(?:@gendoc) (.*)$/gim, function (line, match, index) {
@@ -31,55 +27,17 @@ jadeTemplate = jadeTemplate.replace(/(?:@gendoc) (.*)$/gim, function (line, matc
     return 'div.include!= globals.modules["' + label + '"]';
 });
 
-async.forEach(includes, function (repo, cb) {
-    if (repo.path.slice(0, 4) === 'http') {
-        request(repo.path, function (err, res, body) {
-            if (err) throw err;
-            repo.html = body;
-            cb(null);
-        });
-    } else {
-        fs.readFile(__dirname + '/' + repo.path, 'utf8', function (err, body) {
-            if (err) throw err;
-            repo.html = body;
-            cb(null);
-        });
-    }
-}, function () {
-    var hiddenRE = /(<\!\-\-+ *starthide *\-\-+>)([.\s\S\n]*?)(<\!\-\-+ *endhide *\-\-+>)/gim;
-    var headings = /^(#+)[A-Za-z0-9_ ]/gim;
-    var readmes = {};
 
-    includes = includes.map(function (include) {
-        // strip out hidden stuff
-        var cleaned = include.html.replace(hiddenRE, '');
-        include.json = generateJSON(cleaned, include.label, {version: ''});
-        // indent headings one extra step so we don't have h1s
-        cleaned = cleaned.replace(headings, function (match, group) {
-            return '#' + match;
-        });
-        include.html = generateHtml(cleaned);
-        readmes[include.label] = include.html;
-        return include;
-    });
+getModules(pack.coreModules, function (err, modules) {
 
-    //console.log(intro);
-
-    templateGlobals.modules = readmes;
-    templateGlobals.intro = intro;
+    templateGlobals.modules = modules;
 
     jade.render(jadeTemplate, {
         globals: templateGlobals,
         pretty: true,
-        filename: __dirname + '/index.jade'
+        filename: __dirname + '/docs/index.jade'
     }, function (err, html) {
         if (err) throw err;
-        fs.writeFileSync(__dirname + '/index.html', html, 'utf8');
-    });
-
-    async.forEach(includes, function (include, cb) {
-        fs.writeFile(__dirname + '/json/' + include.label + '.json', JSON.stringify(include.json, null, 2), function (err) {
-            if (err) throw err;
-        });
+        fs.writeFileSync(__dirname + '/docs/index.html', html, 'utf8');
     });
 });
