14e93cc725fefe83f242d923f5204df7ecbbef38
Henrik Joreteg <hjoreteg@gmail.com>
2014-06-18 12:46:23 -0700

    add a few 'learn' pages and the code to generate them

--- build.js
+++ build.js
@@ -1,7 +1,10 @@
 var pack = require('./package.json');
+var path = require('path');
+var rimraf = require('rimraf');
 var fs = require('fs');
 var jade = require('jade');
 var marked = require('marked');
+var metaMarked = require('meta-marked')
 var async = require('async');
 var templateGlobals = {};
 var includes = [];
@@ -12,6 +15,7 @@ var _ = require('underscore');
 
 console.log("INTRO", intro.length);
 
+// build docs pages from npm for core modules
 getModules(pack.coreModules, function (err, modules) {
 
     templateGlobals.modules = modules;
@@ -26,13 +30,13 @@ getModules(pack.coreModules, function (err, modules) {
     });
 });
 
+// grab "latest" updated modules for home page aside.
 getModules(pack.coreModules.concat(pack.formModules), function (err, modules) {
     var recentlyUpdated = _.sortBy(modules, function (module) {
         var latest = module['dist-tags'].latest;
         var date = new Date(module.time[latest]);
         return date;
     }).reverse().slice(0, 3);
-
 
     jade.render(fs.readFileSync(__dirname + '/index.jade', 'utf8'), {
         globals: {recent: recentlyUpdated},
@@ -42,5 +46,54 @@ getModules(pack.coreModules.concat(pack.formModules), function (err, modules) {
         if (err) throw err;
         fs.writeFileSync(__dirname + '/index.html', html, 'utf8');
     });
+});
 
-})
+// build out "learn" pages from learn_markdown directory
+
+// grab them/parse them
+var parsed = fs.readdirSync(__dirname + '/learn_markdown')
+    .filter(isMarkdown)
+    .map(parse)
+    .sort(compare)
+
+
+function parse(input) {
+    var file = metaMarked(fs.readFileSync(__dirname + '/learn_markdown/' + input, 'utf8'));
+    var basename = path.basename(input, '.md');
+    file.url = (basename === 'index') ? '' : basename;
+    return file;
+}
+
+function compare(a, b) {
+    if (a.meta.order < b.meta.order) return -1;
+    if (a.meta.order > b.meta.order) return 1;
+    return 0;
+}
+
+function isMarkdown(file) {
+    return path.extname(file) === '.md';
+}
+
+rimraf.sync(__dirname + '/learn');
+fs.mkdirSync(__dirname + '/learn');
+
+parsed.forEach(function (item) {
+    jade.render(fs.readFileSync(__dirname + '/templates/learn-page.jade', 'utf8'), {
+        globals: {
+            content: item.html,
+            pages: parsed,
+            pageTitle: item.meta.pagetitle,
+            description: item.meta.pagedescription
+        },
+        pretty: true,
+        filename: __dirname + '/index.jade',
+        basedir: __dirname
+    }, function (err, html) {
+        if (err) throw err;
+        var directory = __dirname + '/learn/' + (item.url ? item.url + '/' : '');
+        try { fs.mkdirSync(directory); } catch (e) {}
+        var file = directory + 'index.html';
+        console.log('building: ' + file);
+        fs.writeFileSync(file, html, 'utf8');
+    });
+});
---
+++ learn_markdown/index.md
@@ -0,0 +1,22 @@
+---
+pagetitle: Quick Start Guide
+order: 0
+---
+
+# Jump right in
+
+1. Make sure you've got [node.js installed](http://nodejs.org/).
+2. Install the ampersand command line and running by running this in a terminal:
+```
+npm install ampersand -g
+```
+3. Change into whatever directory you normally put your projects and just run this:
+```
+ampersand
+```
+
+From here, you should be able to follow the prompts to generate a working app.
+
+This app contains what is considered to be best practices for building ampersand apps.
+
+If you hit any snags or want more info [see the docs for the CLI tool](/docs/#ampersand).
